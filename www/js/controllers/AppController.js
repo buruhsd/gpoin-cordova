b6e5f068f8f7deecbe2d217bf6129f54a0c0db48
buruhsd <buruhsd@outlook.co.id>
2017-07-25 14:32:08 +0700

    Get data session and get to view

--- www/js/controllers/AppController.js
+++ www/js/controllers/AppController.js
@@ -1,17 +1,12 @@
-app.controller('AppController', ['$scope','LoginFactory','SessionFactory', function ($scope,LoginFactory,SessionFactory) {
+app.controller('AppController',function($scope) {
 	
 	// LoginFactory.isLogin();
-
-	if(SessionFactory.getSession()){
-		$scope.isLogin = true;
-		console.log($scope.isLogin);
-		// var myStarted = document.getElementById('myStarted');
-		// return myStarted.resetToPage('html/homepage.html', { animation: 'fade' });
-	}else{
-		$scope.isLogin = false;
-		console.log($scope.isLogin);
-		// var myStarted = document.getElementById('myStarted');
-		// return myStarted.resetToPage('html/getstarted.html', { animation: 'fade' });
-	}
+	$scope.name = localStorage.name;
+	$scope.deposite = localStorage.deposite;
 	
-}]);
+
+	console.log(localStorage);
+
+	console.log(localStorage.name);
+	
+});
